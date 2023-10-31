17ba1fe0349a499f007b20839d34d0117f65e643
Ron Mapue <ronmapue@Rons-MacBook-Air.local>
2023-10-31 05:00:21 -0700

    day6: /tsm connect to mongodb

--- typescript_mongodb_skeleton/src/index.ts
+++ typescript_mongodb_skeleton/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import mongoose, { mongo } from 'mongoose'
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -6,6 +7,10 @@ const port = process.env.PORT || 3000
 app.get('/health', (req, res) => {
   res.send(`OK`)
 })
+
+mongoose.connect('mongodb://127.0.0.1:27017').then(() => {
+  console.log(`📄 MongoDB connected on: ${process.env.MONGO_URL || 'mongodb://127.0.0.1:27017'}`)
+}).catch(console.error)
 
 app.listen(port, () => {
   console.log(`🚀 Typescript Starter Kit running on port:: ${port}`)
