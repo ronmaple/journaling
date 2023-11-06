d5d6ccc552b5a97c5dca639b954cfc9c378f0a99
Ron Mapue <ronmapue@Rons-MacBook-Air.local>
2023-11-06 07:30:30 -0800

    day12: 739-better

--- leetcode/739.daily_temperatures.medium.js
+++ leetcode/739.daily_temperatures.medium.js
@@ -23,3 +23,21 @@ var dailyTemperatures = function(temperatures) {
 
   return days
 };
+
+// watched a video to get the idea
+// 208 ms, 65.66mb
+// 1. monotonic decreasing stack (a stack that contains similar conditions -- ie they're all decreasing)
+var dailyTemperatures = function(temperatures) {
+  const days = Array(temperatures.length).fill(0)
+
+  const stack = []
+  for (let i = 0; i < temperatures.length; i++) {
+    while (stack.length && temperatures[stack[stack.length - 1]] < temperatures[i]) {
+      const j = stack.pop()
+      days[j] = i - j
+    }
+    stack.push(i)
+  }
+
+  return days
+};
