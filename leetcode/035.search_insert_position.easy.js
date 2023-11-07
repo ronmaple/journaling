b66f1730a4ca6193d40d33a83be1b70603ab1bf6
Ron Mapue <ronmapue@Rons-MacBook-Air.local>
2023-11-07 10:16:50 -0800

    day13- 035 - better

--- leetcode/035.search_insert_position.easy.js
+++ leetcode/035.search_insert_position.easy.js
@@ -21,3 +21,27 @@ var searchInsert = function(nums, target) {
   }
   return closestIndex
 };
+// binary search O(log n)
+var searchInsert = function(nums, target) {
+  let forward = 0
+  let backward = nums.length - 1
+  let mid = Math.floor((backward + forward) / 2)
+  // to handle scenario where target is not in array...
+  // where we allow forward and backward to overlap
+  // to be able to update mid to a value closer to the target
+  while (forward <= backward) { 
+    mid = Math.floor((backward + forward) / 2)
+    if (nums[mid] === target) {
+      return mid
+    }
+    if (nums[mid] < target) {
+      forward = mid + 1
+    } else {
+      backward = mid - 1
+    }
+  }
+  if (nums[mid] < target) {
+    mid = mid + 1
+  }
+  return mid
+};
