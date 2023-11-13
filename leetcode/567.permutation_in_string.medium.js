5790ad4c72d8f97d8b7f59ccf07f84b74ee35ae8
Ron Mapue <ronmapue@Rons-MacBook-Air.local>
2023-11-13 07:20:37 -0800

    day19: 567 - sliding window

--- leetcode/567.permutation_in_string.medium.js
+++ leetcode/567.permutation_in_string.medium.js
@@ -25,3 +25,47 @@ var checkInclusion = function (s1, s2) {
   }
   return false
 }
+
+
+// sliding window
+// similar checking idea, different technique as above
+// where we slide the window to the right if the condition is not met
+// AND if the next char is present in s1Map but already exceeds the count
+var checkInclusion = function (s1, s2) {
+  const s1Map = {}
+  for (let i = 0; i < s1.length; i++) {
+    const l = s1[i]
+    if (s1Map[l]) {
+      s1Map[l]++
+    } else {
+      s1Map[l] = 1
+    }
+  }
+
+  let left = 0
+  let right = 0
+  let remaining = s1.length
+
+  while (right < s2.length) {
+    if (s1Map[s2[right]] > 0) {
+      remaining--
+    }
+    s1Map[s2[right]]--
+    right++
+
+    if (remaining === 0) {
+      return true
+    }
+
+    if (right - left === s1.length) {
+      // slide window to the right
+      if (s1Map[s2[left]] >= 0) {
+        remaining++
+      }
+      s1Map[s2[left]]++
+      left++
+    }
+  }
+
+  return false
+}
