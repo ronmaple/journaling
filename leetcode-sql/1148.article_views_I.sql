049ac31e9a8a2ff7617e8a06ef3796eff4f15e9b
Ron Mapue <ronmapue@Rons-MacBook-Air.local>
2023-12-06 15:47:39 -0800

    branch1: 1148- make faster

--- leetcode-sql/1148.article_views_I.sql
+++ leetcode-sql/1148.article_views_I.sql
@@ -3,3 +3,11 @@ SELECT
 FROM Views 
 WHERE author_id = viewer_id
 ORDER BY id ASC
+
+# This is faster:
+
+SELECT 
+  DISTINCT v.author_id as id
+FROM Views v
+JOIN Views on v.author_id = v.viewer_id
+ORDER BY id ASC
