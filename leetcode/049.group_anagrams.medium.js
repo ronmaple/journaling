7095f06068a0bbf36ea9f5ff0ababae84fb51d16
Ron Mapue <ronmapue@Rons-MacBook-Air.local>
2023-12-07 07:35:52 -0800

    branch2: 49-group anagram, redo

--- leetcode/049.group_anagrams.medium.js
+++ leetcode/049.group_anagrams.medium.js
@@ -28,3 +28,23 @@ var groupAnagrams = function(strs) {
 
   return output
 };
+
+
+// other solution: character frequency
+var groupAnagrams = function (strs) {
+  const anagrams = {}
+
+  for (const str of strs) {
+    let count = new Array(26).fill(0)
+    for (const char of str) {
+      count[char.charCodeAt() - 97]++
+    }
+    count = count.join(',') // to distinguish a combination like [0, 1, 0] from [0, 10, 0]
+    if (!anagrams[count]) {
+      anagrams[count] = []
+    }
+    anagrams[count].push(str)
+  }
+
+  return Object.values(anagrams)
+}
