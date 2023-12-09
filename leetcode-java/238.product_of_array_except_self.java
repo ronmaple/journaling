83edc2cbb1dbbc94582b8c8da57c70fab2ab71a7
Ron Mapue <ronmapue@Rons-MacBook-Air.local>
2023-12-09 06:38:39 -0800

    branch4: revise 238 java

--- leetcode-java/238.product_of_array_except_self.java
+++ leetcode-java/238.product_of_array_except_self.java
@@ -3,17 +3,17 @@ class Solution {
       int[] products = new int[nums.length];
       Arrays.fill(products, 1);
 
-      int[] leftProduct = new int[]{1};
-      int[] rightProduct = new int[]{1};
+      int leftProduct = 1;
+      int rightProduct = 1;
 
       for (int left = 0; left < nums.length; left++) {
         int right = nums.length - 1 - left;
 
-        products[left] = leftProduct[0] * products[left];
-        products[right] = rightProduct[0] * products[right];
+        products[left] = leftProduct * products[left];
+        products[right] = rightProduct * products[right];
         
-        leftProduct[0] = leftProduct[0] * nums[left];
-        rightProduct[0] = rightProduct[0] * nums[right];
+        leftProduct = leftProduct * nums[left];
+        rightProduct = rightProduct * nums[right];
       }
 
       return products;
