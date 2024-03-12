49873c752ac18d94b49767ed27f71893ca6267d4
Ron <31875601+ronmaple@users.noreply.github.com>
2024-03-12 12:38:24 -0700

    2023-03-12: design/ command

---
+++ design-patterns/javascript/command.js
@@ -0,0 +1,85 @@
+// Problem
+/*
+1. name change
+2. usage change
+etc
+*/
+class OrderManager1 {
+  constructor() {
+    this.orders = [];
+  }
+
+  placeOrder(order, id) {
+    this.orders.push(id);
+    return `You have successfully ordered ${order} (${id})`;
+  }
+
+  trackOrder(id) {
+    return `Your order ${id} will arrive in 20 minutes.`;
+  }
+
+  cancelOrder(id) {
+    this.orders = this.orders.filter((order) => order.id !== id);
+    return `You have canceled your order ${id}`;
+  }
+}
+const manager = new OrderManager();
+
+manager.placeOrder("Pad Thai", "1234");
+manager.trackOrder("1234");
+manager.cancelOrder("1234");
+
+// Solution: Command Pattern
+/*
+1. single method: 'execute', that will execute any command it's given
+*/
+
+/*
+  Pros:
+  - decoupled methods methods from the operation
+  - more control of the commands
+
+  Cons:
+  - boilerplate 
+*/
+class OrderManager {
+  constructor() {
+    this.orders = [];
+  }
+
+  execute(command, ...args) {
+    return command.execute(this.orders, ...args);
+  }
+}
+
+class Command {
+  constructor(execute) {
+    this.execute = execute;
+  }
+}
+
+function PlaceOrderCommand(order, id) {
+  return new Command((orders) => {
+    orders.push(id);
+    return `You have successfully ordered ${order} (${id})`;
+  });
+}
+
+function CancelOrderCommand(id) {
+  return new Command((orders) => {
+    orders = orders.filter((order) => order.id !== id);
+    return `You have canceled your order ${id}`;
+  });
+}
+
+function TrackOrderCommand(id) {
+  return new Command(() => `Your order ${id} will arrive in 20 minutes.`);
+}
+
+const manager = new OrderManager();
+
+manager.execute(new PlaceOrderCommand("Pad Thai", "1234"));
+manager.execute(new TrackOrderCommand("1234"));
+manager.execute(new CancelOrderCommand("1234"));
+Open CodeSandbox
+
