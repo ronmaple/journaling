9b686f04cdbe756f652645b9d1b339dd6ba9caf9
Ron <31875601+ronmaple@users.noreply.github.com>
2024-03-20 09:06:43 -0700

    2024-03-20: mediator or middleware

---
+++ design-patterns/mediator.js
@@ -0,0 +1,29 @@
+// components interact with each other through a central point
+// ie middlewares
+// Ex Use Case: Chat Room
+class ChatRoom {
+  logMessage(user, message) {
+    const time = new Date();
+    const sender = user.getName();
+
+    console.log(`${time} [${sender}]: ${message}`);
+  }
+}
+
+class User {
+  constructor(name, chatroom) {
+    this.name = name;
+    this.chatroom = chatroom;
+  }
+
+  getName() {
+    return this.name;
+  }
+
+  send(message) {
+    this.chatroom.logMessage(this, message);
+  }
+}
+
+// another example:
+// express middlewares
